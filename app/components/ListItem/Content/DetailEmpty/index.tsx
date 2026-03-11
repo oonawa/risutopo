9040cf634dff68cd353e1a58382c3a4aa968d4cd
oonawa <nawaryoga@gmail.com>
2026-03-11 11:46:09 +0900

    ルーレット用リストアイテムのスタイル調整
    feat: ルーレット用リストアイテムのStorybook
    
    fix: detailsなしのタイトル表示からpaddingを削除

--- app/components/ListItem/Content/DetailEmpty/index.tsx
+++ app/components/ListItem/Content/DetailEmpty/index.tsx
@@ -7,7 +7,7 @@ export default function MovieDetailEmpty({ title, children }: Props) {
 	return (
 		<div className="w-full h-full p-4 grid place-items-center">
 			<div className="flex flex-col items-center gap-4">
-				<h2 className="text-xl font-bold text-foreground-dark-1 pt-8">
+				<h2 className="text-xl font-bold text-foreground-dark-1">
 					{title}
 				</h2>
 				{children}
